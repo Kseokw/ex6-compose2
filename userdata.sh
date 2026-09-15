b808c3c6917373cb4440c61faf673c398e0e4d63
Kseokw <tjrdn9306@gmail.com>
2026-09-15 15:24:37 +0900

    config파일업로드

--- userdata.sh
+++ userdata.sh
@@ -7,6 +7,10 @@ mkdir -p /home/ec2-user/nginx/logs
 mkdir -p /home/ec2-user/fastapi/app
 mkdir -p /home/ec2-user/fastapi/logs
 
+# 4. 소유권 변경
+chown -R ec2-user:ec2-user /home/ec2-user/nginx
+chown -R ec2-user:ec2-user /home/ec2-user/fastapi
+
 
 # 3. S3에서 index.html 과 docker-compose.yaml 다운로드
 cd /home/ec2-user/nginx
@@ -16,9 +20,7 @@ aws s3 cp s3://std07-git-web-s3/web-compose/docker_compose_nginx.yaml .
 cd /home/ec2-user/fastapi
 aws s3 cp s3://std07-git-web-s3/web-compose/docker_compose_fastapi.yaml .
 
-# 4. 소유권 변경
-chown -R ec2-user:ec2-user /home/ec2-user/nginx
-chown -R ec2-user:ec2-user /home/ec2-user/fastapi
+
 
 # 5. ECR 로그인 및 컨테이너 실행
 aws ecr get-login-password --region ap-southeast-1 | docker login --username AWS --password-stdin 925047940866.dkr.ecr.ap-southeast-1.amazonaws.com
